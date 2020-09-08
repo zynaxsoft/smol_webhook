b674984e438105013f9357081cc41462f78e5036
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2020-09-08 13:28:20 +0900

    some update from production

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
-use smol_webhook::{ThreadPool, Config};
+use smol_webhook::{Config, ThreadPool};
 
-use std::sync::Arc;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::str;
 use std::process::Command;
+use std::str;
+use std::sync::Arc;
 
-extern crate serde_json;
 extern crate crypto;
+extern crate serde_json;
 
-use serde_json::{Value};
-use crypto::sha1::Sha1;
 use crypto::hmac::Hmac;
 use crypto::mac::{Mac, MacResult};
+use crypto::sha1::Sha1;
 use crypto::util::fixed_time_eq;
+use serde_json::Value;
 
 fn main() {
     let config: Arc<Config> = Arc::new(Config::new().unwrap());
-    let listener = TcpListener::bind(&config.socket_addrs).unwrap();
     println!("Listening on {}", config.socket_addrs);
+    let listener = TcpListener::bind(&config.socket_addrs).unwrap();
 
     let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
@@ -34,23 +34,24 @@ fn main() {
 
 fn run_script(path: &str) {
     let output = Command::new("bash")
-                         .arg(path)
-                         .output()
-                         .expect("failed to execute process");
+        .arg(path)
+        .output()
+        .expect("failed to execute process");
     println!("output\n{}", str::from_utf8(&output.stdout).unwrap());
 }
 
 fn sha1_str_to_u8(s: &str) -> Result<Vec<u8>, &'static str> {
     if s.len() != 45 {
-        return Err("Invalid x-hub-signature string")
+        return Err("Invalid x-hub-signature string");
     }
     match (5..45)
         .step_by(2)
         .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect() {
-            Ok(u8_vec) => Ok(u8_vec),
-            Err(_) => Err("Invalid x-hub-signature string"),
-        }
+        .collect()
+    {
+        Ok(u8_vec) => Ok(u8_vec),
+        Err(_) => Err("Invalid x-hub-signature string"),
+    }
 }
 
 fn check_hash(local_key: &str, body: &str, signature: &str) -> bool {
@@ -71,7 +72,7 @@ fn get_header<'a>(key: &str, headers: &'a str) -> Option<&'a str> {
     for header in headers.split("\r\n") {
         if header.starts_with(key) {
             let value = header.split(":").last().unwrap_or("").trim();
-            return Some(value)
+            return Some(value);
         }
     }
     None
@@ -83,7 +84,7 @@ fn process_event(config: Arc<Config>, request: &str) {
     if let Some(signature) = get_header("X-Hub-Signature", &request) {
         if !check_hash(&config.secret_key, &body, signature) {
             println!("Invalid signature. Ignoring this event.");
-            return
+            return;
         }
     }
     println!("Got request \n{}", request);
@@ -91,17 +92,19 @@ fn process_event(config: Arc<Config>, request: &str) {
     // println!("{:?}", json);
     if let Value::String(git_ref) = &json["ref"] {
         if &format!("refs/heads/{}", &config.branch) != git_ref {
-            println!("Pushed branch \"{}\" doesn't match the configured branch \"{}\"",
-                     git_ref, &config.branch);
+            println!(
+                "Pushed branch \"{}\" doesn't match the configured branch \"{}\"",
+                git_ref, &config.branch
+            );
             println!("Ignoring this event.");
-            return
+            return;
         }
     }
     run_script(&config.script_path);
 }
 
 fn handle_connection(config: Arc<Config>, mut stream: TcpStream) {
-    const BUFF_SIZE: usize = 1024;
+    const BUFF_SIZE: usize = 20024;
     let mut buffer = [0; BUFF_SIZE];
     let mut read_bytes = stream.read(&mut buffer).unwrap();
     let mut request = String::new();
